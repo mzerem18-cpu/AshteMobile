adc6ddfe3bacc16198cb4215cc975b09828eca2d
mzerem18-cpu <mzerem18@gmail.com>
2026-05-13 04:45:07 +0300

    Create AppsViewModel.swift

---
+++ Feather/Views/Apps/AppsViewModel.swift
@@ -0,0 +1,40 @@
+import Foundation
+import SwiftUI
+
+class AppsViewModel: ObservableObject {
+    @Published var apps: [RemoteApp] = []
+    @Published var isLoading = false
+    @Published var errorMessage: String?
+    
+    func fetchApps() {
+        isLoading = true
+        errorMessage = nil
+        
+        guard let url = URL(string: "https://ashtemobile.site/ipaas.json") else {
+            self.errorMessage = "Invalid URL"
+            self.isLoading = false
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                self.isLoading = false
+                
+                if let error = error {
+                    self.errorMessage = error.localizedDescription
+                    return
+                }
+                
+                guard let data = data else { return }
+                
+                do {
+                    // JSONـەکە دەخوێنێتەوە و دەیکات بە لیستی یارییەکان
+                    let decodedData = try JSONDecoder().decode([RemoteApp].self, from: data)
+                    self.apps = decodedData
+                } catch {
+                    self.errorMessage = "Failed to decode JSON: \(error.localizedDescription)"
+                }
+            }
+        }.resume()
+    }
+}
